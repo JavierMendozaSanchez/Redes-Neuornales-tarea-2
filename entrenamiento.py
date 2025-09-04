c2cc229226b919ca073b51886372f6c72c0a2f96
JavierMS <javimesa16@gmail.com>
2025-09-03 21:26:38 -0600

    Codigo que permite a mi red ser entrenada como cambie el nombre a mi network tuve que modificar esto

--- entrenamiento.py
+++ entrenamiento.py
@@ -1,8 +1,9 @@
 
 #Este codigo esta basado en el repositorio de MichalDanielDobrzanski
 #librerias a utilizar 
+
 import mnist_loader
-import network
+import network_crossentropy #importamos ahora nuestra red con el crossentrpy
 import pickle
 #Parte del codigo que permite importar el archivo de imagenes para entrenar
 training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
@@ -11,10 +12,13 @@ training_data = list(training_data)
 print(" Datos cargados correctamente")
 
 #Parte del codigo que permite entrenar a la red
-net = network.Network([784, 30, 10])
+net = network_crossentropy.Network([784, 30, 10]) #aqui tambien
 net.SGD(training_data, 30, 10, 0.5, test_data=test_data)
 #guardamos nuestra red
-with open("red_prueba.pkl", "wb") as f:
+with open("red_pruebaEntropy.pkl", "wb") as f:
     pickle.dump(net, f)
 #damos el mensaje que despues de todas las epocas la red ha sido entrenada
-print("Entrenamiento terminado y red guardada en red_prueba.pkl")
+print("Entrenamiento terminado y red guardada en red_pruebaEntropy.pkl")
+#nota: compare que sin el cross entropy me daba 94 porciento y ahorita 
+#me esta dando casi un 95.17 porciento para mi es algo muy bueno de ver 
+#pues con el learning rate no llegaba a esto
